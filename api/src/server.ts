34459a480119f6678fbcbce673d53c8b5b248d1a
matheusrodacki <matheus.rodacki@outlook.com>
2025-11-08 10:56:56 -0300

    feat: :sparkles: add first listwebhooks route

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,16 +1,26 @@
 import { fastifyCors } from "@fastify/cors";
-import { fastifySwagger } from "@fastify/swagger";
-import { fastify } from "fastify";
+import fastifySwagger, { type SwaggerOptions } from "@fastify/swagger";
+import {
+  fastify,
+  type FastifyPluginCallback,
+  type RawServerDefault,
+} from "fastify";
 import {
   jsonSchemaTransform,
-  ZodTypeProvider,
-} from "fastify-type-provider-zod";
-import {
+  type ZodTypeProvider,
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
+import ScalarApiReference from "@scalar/fastify-api-reference";
+import { listWebhooks } from "./routes/list-webhooks";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+const swaggerPlugin = fastifySwagger as FastifyPluginCallback<
+  SwaggerOptions,
+  RawServerDefault,
+  ZodTypeProvider
+>;
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -21,19 +31,22 @@ app.register(fastifyCors, {
   //   credentials: true,
 });
 
-app.register(fastifySwagger, {
+app.register(swaggerPlugin, {
   openapi: {
     info: {
       title: "Webhook Inspector API",
       description: "Api for capturing and inspecting webhook requests",
-      versions: "1.0.0",
+      version: "1.0.0",
     },
   },
-  transform: {
-    jsonSchemaTransform,
-  },
+  transform: jsonSchemaTransform,
 });
+
+app.register(ScalarApiReference, { routePrefix: "/docs" });
+
+app.register(listWebhooks);
 
 app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP server running on http://localhost:3333");
+  console.log("Docs available on http://localhost:3333/docs ");
 });
